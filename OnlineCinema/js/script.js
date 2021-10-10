1ef2d4d77a199cc9a0906c69f347df03051dcce8
rskalex <51515895+rskalex@users.noreply.github.com>
2021-10-10 22:53:19 +0300

    add js file

---
+++ OnlineCinema/js/script.js
@@ -0,0 +1,18 @@
+const schemeSvg = document.querySelector('.scheme-svg'),
+      price = document.querySelector('.price-total'),
+      cost = 12,
+      btn = document.querySelector('.btn');
+let totalPrice = 0;
+
+price.textContent = totalPrice;
+
+schemeSvg.addEventListener('click', (e) => {
+    e.preventDefault();
+    let target = e.target;
+    if (!target.classList.contains('booked')) {
+        target.classList.toggle('active');
+        totalPrice += cost;
+        price.textContent = totalPrice;
+    }
+    
+});

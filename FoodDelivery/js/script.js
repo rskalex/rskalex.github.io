a3e654428bf0f1bb3d0aababd4fcf95e1b7fd057
rskalex <51515895+rskalex@users.noreply.github.com>
2021-10-31 23:06:15 +0300

    Update script.js

--- FoodDelivery/js/script.js
+++ FoodDelivery/js/script.js
@@ -71,7 +71,7 @@ function slider(container, slide, nextArr, prevArr, wrapper, field) {
     sliderField.style.maxWidth = Math.floor(+width.slice(0, width.length-2)) * 3 + 'px';
     sliderField.style.display = 'flex';
     sliderField.style.transition = '0.5s all';
-    console.log(sliderField.style.width)
+    alert(sliderField.style.width)
     slider.style.position = 'relative';
 
     function deleteNotNum(string) {

21323ba2c0c7a80697b4782dca6fb8f450375606
rskalex <51515895+rskalex@users.noreply.github.com>
2022-04-27 23:28:18 +0300

    feat: add new project

---
+++ alex123/js/script.js
@@ -0,0 +1,91 @@
+//menu
+let burger = document.querySelector('.menu-burger');
+let body = document.querySelector('.body');
+let menu = document.querySelector('.header-navbar');
+let logo = document.querySelector('.header-logo');
+let wrapper = document.querySelector('.wrapper');
+const menuItem = document.querySelectorAll('.menu-item');
+
+document.documentElement.addEventListener('click', (e) => {
+    if(!e.target.closest('.header-wrapper')){
+        burger.classList.remove('_active');
+        menu.classList.remove('_active');
+        logo.classList.remove('_hide');
+        wrapper.classList.remove('_active');
+        body.classList.remove('lock')
+    }
+});
+
+burger.addEventListener('click', function(e){
+    logo.classList.toggle('_hide');
+    menu.classList.toggle('_active');
+    burger.classList.toggle('_active');
+    body.classList.toggle('lock');
+    wrapper.classList.toggle('_active')
+
+    menuItem.forEach((item, i) => {
+        item.addEventListener('click', (e) => {
+            if(e.target){
+                menu.classList.remove('_active');
+                body.classList.remove('lock');
+                logo.classList.remove('_hide');
+                burger.classList.remove('_active');
+                wrapper.classList.remove('_active')
+            }
+        })
+        if(body.classList.contains('lock')){
+            item.style.cssText = `font-size: 32px; margin-right: 0`;
+        } else {
+            item.style.cssText = `font-size: 15px;`;
+        }
+    })
+});
+
+let acc = document.querySelectorAll(".accordion");
+
+
+menu.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target.parentNode.classList.contains('menu-item')){
+        menuItem.forEach((item, i) => item.classList.remove('active'))
+        target.parentNode.classList.add('active')
+    }
+})
+
+menuItem.forEach((item, i) =>{
+    item.addEventListener('click', (e) => {
+        let target = e.target;
+        if(target){
+            // acc[i].classList.add("active")
+            console.log(acc[i])
+            let panel = acc[i].nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        }
+    })
+})
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+let panel = document.querySelectorAll('p'),
+    width = window.getComputedStyle(document.querySelector('.wrapper')).width
+    console.log(width)
+if(+width.slice(0, width.length - 2) <= 768){
+    console.log(panel)
+    panel.forEach(item => {
+        item.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis debitis doloribus nam a pariatur necessitatibus delectus, atque maiores' + '...'
+    })
+}
